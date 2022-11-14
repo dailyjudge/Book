4a81c3c10c8dc5ff187cfa70661d1834e79268e3
dailyjudge <jmk961202@gmail.com>
2022-11-14 18:05:32 +0900

    test

--- src/main/java/com/book/main/HC.java
+++ src/main/java/com/book/main/HC.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HC")
 public class HC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		request.setAttribute("contentPage", "jsp/bb.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
 	}
 
