a2e4ea2754e31fb1edf65e1e4c72f8fd5155af4c
dailyjudge <jmk961202@gmail.com>
2022-11-15 22:09:08 +0900

    book-search-css

--- src/main/webapp/js/book_search.js
+++ src/main/webapp/js/book_search.js
@@ -2,23 +2,33 @@ $(document).ready(function () {
     var pageNum = 1;
 
     $("#search").click(function () {
-        $("div").html("");
-
+       $("#search-result").html("");
+		
         $.ajax({
             method: "GET",
             url: "https://dapi.kakao.com/v3/search/book?target=title",
             data: { query: $("#query").val(), page: pageNum},
-            headers: {Authorization: "KakaoAK 362cca98cf4fe85293ae140cbbcadeff"} // ########부분에 본인의 REST API 키를 넣어주세요.
-
+            headers: {Authorization: "KakaoAK 362cca98cf4fe85293ae140cbbcadeff"}, // ########부분에 본인의 REST API 키를 넣어주세요.
         })
         .done(function (msg) {
             console.log(msg);
-            for (var i = 0; i < 10; i++){
-                $("#search-result").append("<h2><a href='"+ msg.documents[i].url +"'>" + msg.documents[i].title + "</a></h2>");
-                $("#search-result").append("<strong>저자:</strong> " + msg.documents[i].authors + "<br>");
-                $("#search-result").append("<strong>출판사:</strong> " + msg.documents[i].publisher + "<br>");
-                $("#search-result").append("<strong>요약:</strong> " + msg.documents[i].contents + "...<br>");
-                $("#search-result").append("<img src='" + msg.documents[i].thumbnail + "'/><br>");
+
+            for (var i = 0; i < 8; i++){
+	
+				$("#search-result").append(`
+				<div class="book-item">
+            		<div class="item-img">
+               		   <img class='thumbnail' src=${msg.documents[i].thumbnail} alt=''>
+           		    </div>
+            		<div class="item-desc">
+               			<p>『${msg.documents[i].title}』</p>
+               			<p>『${msg.documents[i].contents.slice(0, 100)}...』</p>
+              			<p>\\${msg.documents[i].price}</p>
+           		    </div>
+              	</div>    
+           		    `);
+        		
+   				
             }
         });
     })
@@ -33,13 +43,38 @@ $(document).ready(function () {
         })
         .done(function (msg) {
             console.log(msg);
-            for (var i = 0; i < 10; i++){
-                $("#search-result").append("<h2><a href='"+ msg.documents[i].url +"'>" + msg.documents[i].title + "</a></h2>");
-                $("#search-result").append("<strong>저자:</strong> " + msg.documents[i].authors + "<br>");
-                $("#search-result").append("<strong>출판사:</strong> " + msg.documents[i].publisher + "<br>");
-                $("#search-result").append("<strong>요약:</strong> " + msg.documents[i].contents + "...<br>");
-                $("#search-result").append("<img src='" + msg.documents[i].thumbnail + "'/><br>");
+           for (var i = 0; i < 8; i++){
+	
+				$("#search-result").append(`
+				<div class="book-item">
+            		<div class="item-img">
+               		   <img class='thumbnail' src=${msg.documents[i].thumbnail} alt=''>
+           		    </div>
+            		<div class="item-desc">
+               			<p>『${msg.documents[i].title}』</p>
+               			<p>『${msg.documents[i].contents.slice(0, 100)}...』</p>
+              			<p>\\${msg.documents[i].price}</p>
+           		    </div>
+              	</div>    
+           		    `);
+        		
+   				
             }
         });
     })
+    
+
+            $(document).on("mouseover", ".item-img", function(){
+               	console.log(111);
+                $(this).fadeOut('slow');
+                $(this).next().fadeIn('slow');
+                
+            });
+
+            $(document).on("mouseleave",".item-desc", function(){
+                $(this).fadeOut('slow');
+                $(this).prev().fadeIn('slow');
+            });
+
+    
 })
--- src/main/webapp/jsp/kmj&jmk/book_search.jsp
+++ src/main/webapp/jsp/kmj&jmk/book_search.jsp
@@ -5,12 +5,18 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
-
+<script src="https://code.jquery.com/jquery-3.6.1.js"
+		integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="
+		crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/book_search.js"></script>
 </head>
 <body>
+	<div class="test">
 	<input type="text" id="query">
-	<button id="search">검색</button>
-	<div id="search-result"></div>
-	<button id="search-more">더보기</button>	
+	<button id="search">검색</button>	
+	<button id="search-more">더보기</button>
+	</div>
+	<div id="search-result" class="book-container"></div>
+	
 </body>
 </html>
