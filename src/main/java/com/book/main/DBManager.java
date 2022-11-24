8c6de825ce8e4c9ddc07da9dc6c2a503987002e0
kye <kye2644@gmail.com>
2022-11-24 19:57:27 +0900

    no message

--- src/main/java/com/book/main/DBManager.java
+++ src/main/java/com/book/main/DBManager.java
@@ -11,7 +11,7 @@ public static Connection connect() throws SQLException {
 	
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
 
-	return DriverManager.getConnection(url, "c##mk", "mk");
+	return DriverManager.getConnection(url, "c##kye", "kye");
 
 }
 public static void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
