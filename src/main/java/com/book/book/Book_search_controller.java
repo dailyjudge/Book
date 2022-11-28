3152fd5b5c35bf6d3d712b09ca0f74788884a4f2
dailyjudge <jmk961202@gmail.com>
2022-11-28 17:59:53 +0900

    dddd

--- src/main/java/com/book/book/Book_search_controller.java
+++ src/main/java/com/book/book/Book_search_controller.java
@@ -12,11 +12,10 @@ import com.book.account.AccountDAO;
 @WebServlet("/Book_search_controller")
 public class Book_search_controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if (AccountDAO.getAdao().loginCheck(request)) {
+			
+			AccountDAO.getAdao().loginCheck(request);
 			request.setAttribute("contentPage", "jsp/kmj&jmk/book_search.jsp");
 			request.getRequestDispatcher("index.jsp").forward(request, response);			
-		}
-		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
